691fb8d9363674235cdd157942cb90b0a3dfe704
Tomasz Sikora <frakti7@gmail.com>
2015-03-28 20:24:43 +0100

    Add Packer template with CentOS, Guest Additions and nvm
    Running some of the NodeSchool workshoppers can be tricky on Windows
    platform. To decrease preparation time for workshops a VM/OVA generated
    by the template can be used. In addition the VM includes all native
    dependencies the workshoppers (#12).

---
+++ tools/packer/scripts/install-nvm.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install 0.12 # for 'shader-school'
+nvm install iojs
+nvm alias default iojs
