9275822790323ddac6545a4c19e6fd679fbff2b9
Tomasz Sikora <frakti7@gmail.com>
2015-04-19 22:22:55 +0200

    Create alias npm alias to install modules without symlinks (#12)

--- tools/packer/scripts/install-nvm.sh
+++ tools/packer/scripts/install-nvm.sh
@@ -3,3 +3,8 @@ source ~/.nvm/nvm.sh
 nvm install 0.12 # for 'shader-school'
 nvm install iojs
 nvm alias default iojs
+
+# When enabled directory sharing with Windows host you operate on NTFS
+# and some modules might not be able to be installed because of symlink
+# creation not possible on NTFS.
+echo "alias npm='npm --no-bin-links'" >> ~/.bashrc
