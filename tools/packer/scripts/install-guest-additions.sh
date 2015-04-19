54ba99389e9abd8b69deb4782afb0fa7d7a2caa0
Tomasz Sikora <frakti7@gmail.com>
2015-04-19 22:22:05 +0200

    Make sharing working with host without right issues (#12)

--- tools/packer/scripts/install-guest-additions.sh
+++ tools/packer/scripts/install-guest-additions.sh
@@ -3,3 +3,5 @@ mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt/guest
 sh /mnt/guest/VBoxLinuxAdditions.run
 umount /mnt/guest
 rm -rf /home/vagrant/VBoxGuestAdditions.iso /mnt/guest
+
+usermod -a -G vboxsf vagrant
